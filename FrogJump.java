9e3bddba2bd0ed9f02576d57260fef9961ad8a17
Lohith N Negalur <47582005+lokinegalur@users.noreply.github.com>
2021-08-03 19:00:23 +0530

    Update FrogJump.java

--- FrogJump.java
+++ FrogJump.java
@@ -1,36 +1,29 @@
-package practice;
-import java.lang.Math;
-import java.util.Scanner;
-import java.util.Arrays;
+import java.util.*;
+import java.lang.*;
 public class FrogJump{
-	  static int[] dp;
-	  static int F(int[] arr,int i){
-	    if(dp[i]==-1){
-	      if(i==1){
-	        dp[i]=Math.abs(arr[i]-arr[0]);
-	      }
-	      else if(i==2){
-	        int dir = Math.abs(arr[2]-arr[0]);
-	        int indir = Math.abs(arr[2]-arr[1])+Math.abs(arr[1]-arr[0]);
-	        dp[i]=Math.min(dir,indir);
-	      }
-	      else{
-	        int prev1 = F(arr,i-1)+Math.abs(arr[i]-arr[i-1]);
-	        int prev2 = F(arr,i-2)+Math.abs(arr[i]-arr[i-2]);
-	        dp[i]=Math.min(prev1,prev2);
-	      }
-	    }
-	    return dp[i];
-	  }
-	  public static void main(String args[]){
-	    Scanner in = new Scanner(System.in);
-	    int n=in.nextInt();
-	    int[] arr = new int[n];
-	    for(int i=0;i<n;i++)
-	      arr[i]=in.nextInt();
-	    dp=new int[n];
-	    Arrays.fill(dp,-1);
-	   	int minCost = F(arr,n-1);
-	    System.out.println(minCost);
-	  }
+  static int[] dp;
+  
+  static int minC(int[] h,int index){
+    if(dp[index]!=-1)
+      return dp[index];
+    int p=minC(h,index-1)+Math.abs(h[index]-h[index-1]);
+    int v=minC(h,index-2)+Math.abs(h[index]-h[index-2]);
+    dp[index]=Math.min(p,v);
+    return dp[index];
+  }
+  
+  public static void main(String args[]){
+    Scanner in=new Scanner(System.in);
+    int n=in.nextInt();
+    dp=new int[n];
+    int h[]=new int[n];
+    for(int i=0;i<n;i++)
+      h[i]=in.nextInt();
+    Arrays.fill(dp,-1);
+    dp[0]=0;
+    dp[1]=Math.abs(h[1]-h[0]);
+    int ans=minC(h,n-1);
+    System.out.println(ans);
+  }
+  
 }
