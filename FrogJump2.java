4ceb736bd361925c01c05ba26f8de68ae7811a3c
Lohith N Negalur <47582005+lokinegalur@users.noreply.github.com>
2021-08-03 20:03:28 +0530

    Update FrogJump2.java

--- FrogJump2.java
+++ FrogJump2.java
@@ -1,45 +1,33 @@
-/*package whatever //do not write package name here */
+import java.util.*;
+import java.lang.*;
 
-import java.lang.Math;
-import java.util.Scanner;
-import java.io.*;
-class FrogJump2 {
-    static int[] dp;    
-    static int mc(int[] arr,int k,int stone){
-        if(dp[stone]==0){
-            int res;
-            
-        if(stone==0)
-            res= 0;
-            
-        else if(stone==1)
-            res= Math.abs(arr[1]-arr[0]);
-            
-        else{
-        int cost=Integer.MAX_VALUE;
-        int start=0;
-        if(stone>=k)
-            start=stone-k;
-        for(int i=start;i<stone;i++)
-                cost=Math.min(cost,mc(arr,k,i)+Math.abs(arr[stone]-arr[i]));
-          res=cost;
-            }
-           
-        dp[stone]=res;
-        }
-        return dp[stone];
-    }
-	public static void main (String[] args) {
-	    InputStreamReader is = new InputStreamReader(System.in);
-	    BufferedReader buff = new BufferedReader(is);
-	    String input[]=buff.readLine().split();
-	    int n=Integer.parseInt(input[0]);
-	    int k=Integer.parseInt(input[1]);
-		int arr[]=new int[n];
-		input=buff.readLine().split();
-		for(int i=0;i<n;i++) arr[i]=Integer.parseInt(input[i]);
-		dp=new int[n];
-		int minCost=mc(arr,k,n-1);
-		System.out.println(minCost);
-	}
+
+public class FrogJump2{
+  static int[] dp;
+	
+public static int minc(int[] h,int k,int index){        
+  if(dp[index]!=-1)
+    return dp[index];
+  int min_cost=Integer.MAX_VALUE;
+  for(int i=1;i<=Math.min(k,index);i++){
+    min_cost=Math.min(min_cost,minc(h,k,index-i)+Math.abs(h[index]-h[index-i]));
+  }
+  dp[index]=min_cost;
+  return dp[index];
+}
+  
+  public static void main(String[] args){
+    Scanner in = new Scanner(System.in);
+    int n=in.nextInt();
+    int k=in.nextInt();
+    int h[]=new int [n];
+    for(int i=0;i<n;i++)
+      h[i]=in.nextInt();
+    dp=new int[n];
+    Arrays.fill(dp,-1);
+    dp[0]=0;
+    dp[1]=Math.abs(h[1]-h[0]);
+    int res=minc(h,k,n-1);
+    System.out.println(res);
+  }
 }
